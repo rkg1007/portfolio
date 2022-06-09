4b81ce6bb78677cf438f6a8826e9f0575b68882e
rohit <rkg10072000@gmail.com>
2022-06-09 10:30:05 +0530

    created layout of portfolio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 my-14 lg:px-20 px-5">
+      <div className="col-span-12 lg:col-span-3 bg-white rounded-lg">SideBar</div>
+      <div className="col-span-12 lg:col-span-9 bg-white rounded-lg">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
