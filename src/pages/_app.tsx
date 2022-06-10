604364866594ad386772b8ef478b7d32efd7636e
rohit <rkg10072000@gmail.com>
2022-06-10 20:32:45 +0530

    updated _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,12 @@ import SideBar from "../components/Sidebar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="py-7 h-screen bg-gradient-to-r from-green to-blue-400">
-      <div className="grid grid-cols-12 gap-6 lg:px-20 px-5">
-        <div className="col-span-12 lg:col-span-3 bg-white rounded-lg">
+    <div className="h-screen py-7 bg-gradient-to-r from-green to-blue-400 sm:px-20 md:px-20 lg:px-0">
+      <div className="grid grid-cols-12 gap-6 px-5 lg:px-20">
+        <div className="col-span-12 bg-white rounded-lg lg:col-span-3">
           <SideBar />
         </div>
-        <div className="col-span-12 lg:col-span-9 bg-white rounded-lg">
+        <div className="col-span-12 bg-white rounded-lg lg:col-span-9">
           <Component {...pageProps} />
         </div>
       </div>
