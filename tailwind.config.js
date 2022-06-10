54050e17e28c7a966cef7f92e5057469014162bc
rohit <rkg10072000@gmail.com>
2022-06-10 12:08:07 +0530

    added a new custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.tsx",
-    "./src/components/**/*.tsx",
-  ],
+  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          DEFAULT: "#00f260",
+        },
+        dark: {
+          DEFAULT: "#010101",
+          100: "#0a0b0e",
+          200: "#16181d",
+          300: "#16181d",
+          500: "#0f1115",
+          700: "#202125",
+        },
+      },
+    },
   },
   plugins: [],
 };
